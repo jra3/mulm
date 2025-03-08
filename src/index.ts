ddafd7cebb52cb26016c75604d862c098cd47104
John Allen <jallen@braid.ai>
2025-03-07 22:31:33 -0500

    Getting toolchain set up
    I'm shooting for...
    
    - typescript
    - tailwind
    - koa for the route handling
    - pug for templating
    - htmx for the bulk of interactivity
    - sqlite for the database with an eye towards something more robust later

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import Koa from 'koa';
+import Router from 'koa-router';
+import views from 'koa-views';
+import path from 'path';
+
+const app = new Koa();
+const router = new Router();
+
+app.use(
+    views(path.join(__dirname, 'views'), {
+      extension: 'pug',
+    })
+  );
+
+router.get('/', async (ctx) => {
+    await ctx.render('index', { title: 'Home Page', message: 'Welcome to Koa with Pug!' });
+});
+
+app.use(router.routes()).use(router.allowedMethods());
+
+const PORT = process.env.PORT || 4200;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
