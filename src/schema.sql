8118f0472de163b2eafebdae7015d60786a40123
John Allen <jallen@braid.ai>
2025-03-13 10:12:20 -0400

    give up on tom select styling for now, fill out form in db

--- src/schema.sql
+++ src/schema.sql
@@ -2,17 +2,34 @@
 -- > cd ~/mulm
 -- > sqlite3 database.db < src/schema.sql
 
--- # test data
--- > sqlite3 database.sqlite ".mode csv" ".import src/MOCK_DATA.csv submissions"
-
 CREATE TABLE submissions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
 
-    submission_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    member_name TEXT NOT NULL,
-    species_name TEXT NOT NULL,
+    created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    date_approved DATETIME DEFAULT NULL,
+    member_name TEXT NOT NULL,
+    species_type TEXT NOT NULL,
+    species_class TEXT NOT NULL,
+    species_common_name TEXT NOT NULL,
+    species_latin_name TEXT NOT NULL,
+    water_type TEXT NOT NULL,
+    count TEXT NOT NULL,
+
+    tank_size TEXT NOT NULL,
+    filter_type TEXT NOT NULL,
+    water_change_volume TEXT NOT NULL,
+    water_change_frequency TEXT NOT NULL,
+    temperature TEXT NOT NULL,
+    pH TEXT NOT NULL,
+    GH TEXT NOT NULL,
+    specific_gravity TEXT NOT NULL,
+    substrate_type TEXT NOT NULL,
+    substrate_depth TEXT NOT NULL,
+    substrate_color TEXT NOT NULL,
+
+    submitted_on DATETIME DEFAULT NULL,
+    approved_on DATETIME DEFAULT NULL,
     approved_by TEXT DEFAULT NULL,
     points INTEGER DEFAULT NULL
 );
