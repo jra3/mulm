53197d9d049aa5a2459cf6263c4c6c1c7b5e936a
John Allen <jallen@braid.ai>
2025-03-31 12:32:30 -0400

    move unit tests to temp files, better-sqlite3 seems to not support shared in-memory insances

--- src/__tests__/members.ts
+++ src/__tests__/members.ts
@@ -1,22 +1,36 @@
 import Database from 'better-sqlite3';
 import fs from 'fs';
 import path from 'path';
-import { getMembersList } from "../db/members";
+import { createMember, getMembersList } from "../db/members";
 import { setDBFactory } from "../db/conn";
 
-let memoryDb: Database.Database;
+beforeAll(() => {
+	fs.mkdirSync("/tmp/mulm");
+});
+
+let instance = 1;
 beforeEach(() => {
-  memoryDb = new Database(':memory:');
+	const dbUri = `/tmp/mulm/database-${instance++}.sqlite`;
+	const memoryDb = new Database(dbUri);
 	const schema = fs.readFileSync(path.join(__dirname, "../db/schema.sql"), 'utf-8')
 	memoryDb.exec(schema);
-	setDBFactory(() => memoryDb);
-  // Seed data
+	setDBFactory((readonly) => new Database(dbUri, { readonly }));
+	memoryDb.close();
+	// Seed data
 });
 
-test('insert and query', () => {
-	expect(getMembersList()).toStrictEqual([]);
+afterAll(() => {
+	fs.rmdirSync("/tmp/mulm", { recursive: true });
+});
+
+test('Members list append', () => {
+	expect(getMembersList().length).toEqual(0);
+	createMember("honk@dazzle.com", "Honk Dazzle", { google_sub: "123456789" });
+	expect(getMembersList().length).toEqual(1);
 })
 
-afterEach(() => {
-  memoryDb.close();
-});
+/* test('Members list append', () => {
+	createMember("honk@dazzle2.com", "Honk Dazzle", { google_sub: "123456789" });
+	expect(getMembersList().length).toEqual(1);
+})
+ */
