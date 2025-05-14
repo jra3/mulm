2810742372f868a4a3fd9ba934e01950f19a2b88
John Allen <jallen@braid.ai>
2025-05-14 16:56:36 -0400

    Nice email name, cc admins on submission

--- src/notifications.ts
+++ src/notifications.ts
@@ -4,26 +4,32 @@ import { type Submission } from "./db/submissions";
 import { getAdminEmails, MemberRecord } from "./db/members";
 import * as pug from "pug";
 
+const DEBUG_EMAIL = process.env.DEBUG_EMAIL;
+const fromEmail = `BASNY Breeder Awards ${config.fromEmail}`;
+
 const transporter = nodemailer.createTransport({
-  host: config.smtpHost,
-  port: config.smtpPort,
-  secure: config.smtpSecure,
-  auth: {
-    user: config.fromEmail,
-    pass: config.smtpPassword,
-  },
+	host: config.smtpHost,
+	port: config.smtpPort,
+	secure: config.smtpSecure,
+	auth: {
+		user: config.fromEmail,
+		pass: config.smtpPassword,
+	},
 });
+
 
 const renderOnSubmission = pug.compileFile("src/views/email/onSubmission.pug");
 export async function onSubmissionSend(
 	sub: Submission,
 	member: MemberRecord,
 ) {
-	const admins = await getAdminEmails();	
+	const admins = await getAdminEmails();
+
 	return transporter.sendMail({
-		from: config.fromEmail,
+		from: fromEmail,
 		to: member.contact_email,
-		bcc: admins,
+		cc: admins,
+		bcc: DEBUG_EMAIL,
 		subject: `Submission Confirmation - ${sub.species_common_name}`,
 		html: renderOnSubmission({
 			domain: config.domain,
@@ -33,14 +39,16 @@ export async function onSubmissionSend(
 	});
 }
 
+
 const renderOnApprove = pug.compileFile("src/views/email/onApproval.pug");
 export async function onSubmissionApprove(
 	sub: Submission,
 	member: MemberRecord,
 ) {
 	return transporter.sendMail({
-		from: config.fromEmail,
+		from: fromEmail,
 		to: member.contact_email,
+		bcc: DEBUG_EMAIL,
 		subject: `Submission Approved! - ${sub.species_common_name}`,
 		html: renderOnApprove({
 			domain: config.domain,
@@ -50,19 +58,21 @@ export async function onSubmissionApprove(
 	});
 }
 
+
 export async function sendVerificationEmail(email: string, url: string) {
 	return transporter.sendMail({
-		from: config.fromEmail,
+		from: fromEmail,
 		to: email,
 		subject: "Verify your email",
 		text: `Click the link to verify your account and log in: ${url}`,
 	});
 }
 
+
 const renderResetEmail = pug.compileFile("src/views/email/onForgotPassword.pug");
 export async function sendResetEmail(email: string, display_name: string, code: string) {
 	return transporter.sendMail({
-		from: config.fromEmail,
+		from: fromEmail,
 		to: email,
 		subject: "Reset Password",
 		html: renderResetEmail({
@@ -73,11 +83,13 @@ export async function sendResetEmail(email: string, display_name: string, code: 
 	});
 }
 
+
 const renderInviteEmail = pug.compileFile("src/views/email/invite.pug");
 export async function sendInviteEmail(email: string, display_name: string, code: string) {
 	return transporter.sendMail({
-		from: config.fromEmail,
+		from: fromEmail,
 		to: email,
+		bcc: DEBUG_EMAIL,
 		subject: "Welcome to the BASNY Breeders Awards Program!",
 		html: renderInviteEmail({
 			domain: config.domain,
